aedb63a794ef34a387c9d383d7a1272f37824f9c
rok3tt <bzsolczay@gmail.com>
2025-08-17 13:18:13 +1000

    feat(early): added a basic temporary stack test

--- early/entry.c
+++ early/entry.c
@@ -1,4 +1,24 @@
 __attribute__((section(".entry"), naked))
 void _entry(void) {
-  __asm__ volatile("hlt");
+  /*
+	Unstable temporary stack test, remember DRAM
+	has not been trained yet, and many other hardware
+	initializations have not been run.
+
+	In emulaters like QEMU, they don't really support
+	silicon init, memory init, etc. So it's mostly just a
+	guessing game from what is known to work.
+  */
+  __asm__ volatile(
+	"movl 0x4F0, %esp\n"
+	"call test\n"
+    "hlt"
+  );
+}
+
+void test(void) {
+  /*
+	Just to check if calling now works
+	---but it should.
+  */
 }
